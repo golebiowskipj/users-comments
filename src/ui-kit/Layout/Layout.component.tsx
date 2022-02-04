069cf85b04943414da7c08a5ec825cbca7b84d87
golebiowskipj <golebiowskipj@gmail.com>
2022-02-04 14:12:53 +0100

    chore: eslint, prettier, commitlint, husky

---
+++ src/ui-kit/Layout/Layout.component.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+export const Layout = ({ children }: LayoutProps) => {
+  <div>{children}</div>;
+};
---
+++ src/ui-kit/Layout/Layout.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Grid = styled.main`
+padding: 2rem 1rem
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
